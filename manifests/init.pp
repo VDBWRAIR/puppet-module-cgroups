24fea4cc490d3feb2216d9ef37c93370d204a341
Tyghe Vallard <vallardt@gmail.com>
2016-01-11 12:09:42 -0500

    using validate command instead of type function which does not exist anymore

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,14 +10,8 @@ class cgroups (
 ) {
 
   validate_absolute_path($config_file_path)
-
-  if type($service_name) != 'string' {
-    fail('cgroups::service_name must be a string.')
-  }
-
-  if type($package_name) != 'string' and type($package_name) != 'array' {
-    fail('cgroups::package_name must be a string or an array.')
-  }
+  validate_string($service_name)
+  validate_string($package_name)
 
   case $::osfamily {
     'RedHat': {
